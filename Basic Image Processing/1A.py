7724581c6fbd989caeec17749d4e0ccb4b90f5d1
KrithikRajan <91519481+KrithikRajan@users.noreply.github.com>
2023-12-28 22:13:45 +0530

    1A.py

--- Basic Image Processing/1A.py
+++ Basic Image Processing/1A.py
@@ -1,12 +1,9 @@
 import cv2
 import numpy as np
 from PIL import Image
-
-img = cv2.imread("anantha.jpg",0)
+img = cv2.imread("photo-1561731216-c3a4d99437d5.jpg",0)
 equ = cv2.equalizeHist(img)
 res = np.hstack((img,equ))
-
 cv2.imshow('Image',res)
-
 cv2.waitKey(0)
 cv2.destroyWindow()
